ea82fed1c1b817c00737e48a17dce1ca0507ede6
Jayesh <jayeshp.itpath@gmail.com>
2023-04-04 10:09:14 +0530

    #034 UI is allmost completed,form validation completed

---
+++ src/Components/About.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+
+</script>
+
+<div class="grid md:grid-cols gap-2  md:ml-44 w-9/12 pt-44">
+    <p class="text-black font-bold text-6xl">About Us</p><br>
+    <p class="text-black pt-1">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit laboriosam, 
+    quia quaerat explicabo neque ad obcaecati aliquid quas delectus omnis perspiciatis modi tempora dolore incidunt expedita? 
+    Animi tenetur dolorem ipsa corrupti vel sed earum odit. Molestias, libero consequatur! Voluptatem, quam itaque. Perspiciatis 
+    labore commodi veniam, optio quisquam provident veritatis modi minima illum! Harum eaque aliquid temporibus magnam rem perferendis
+</div>
+ 
+<div class="grid md:grid-cols gap-2 mt-8 md:ml-44 w-9/12 pt-10">
+        <div class="lg:w-full  ">
+            <div class="grid md:grid-cols-4 sm:grid-cols-2 grid-cols-1   shadow-lg rounded-lg w-full">
+                <div class="p-4 pb-6 flex justify-center flex-col items-center">
+                    <img class="md:block hidden" src="..\image\Mac.png" alt=" " />
+                    
+                    <p class="font-medium text-xl leading-5 text-gray-800 dark:text-white mt-4">MAC</p>
+                </div>
+                <div class="p-4 pb-6 flex justify-center flex-col items-center">
+                    <img class="md:block hidden" src="..\image\ipad.png" alt=" " />
+                 
+                    <p class="font-medium text-xl leading-5 text-gray-800 dark:text-white mt-4">IPAD</p>
+                </div>
+                
+                <div class="p-4 pb-6 flex justify-center flex-col items-center">
+                    <img class="md:block hidden" src="..\image\iphone.png" alt=" " />
+                   <p class="font-medium text-xl leading-5 text-gray-800 dark:text-white mt-4">IPHONES</p>
+                </div>
+                <div class="p-4 pb-6 flex justify-center flex-col items-center">
+                    <img class="md:block hidden" src="..\image\watch.png" alt=" " />
+                   <p class="font-medium text-xl leading-5 text-gray-800 dark:text-white mt-4">WATCHES</p>
+                </div>
+                <div class="p-4 pb-6 flex justify-center flex-col items-center">
+                    <img class="md:block hidden" src="..\image\Airtags.png" alt=" " />
+                   <p class="font-medium text-xl leading-5 text-gray-800 dark:text-white mt-4">AIRTAGS</p>
+                </div>
+                <div class="p-4 pb-6 flex justify-center flex-col items-center">
+                    <img class="md:block hidden" src="..\image\ipod.png" alt=" " />
+                   <p class="font-medium text-xl leading-5 text-gray-800 dark:text-white mt-4">AIRPODS</p>
+                </div>
+                <div class="p-4 pb-6 flex justify-center flex-col items-center">
+                    <img class="md:block hidden" src="..\image\AppleTV.png" alt=" " />
+                   <p class="font-medium text-xl leading-5 text-gray-800 dark:text-white mt-4">APPLE TV</p>
+                </div>
+                <div class="p-4 pb-6 flex justify-center flex-col items-center">
+                    <img class="md:block hidden" src="..\image\HOMEPads.png" alt=" " />
+                   <p class="font-medium text-xl leading-5 text-gray-800 dark:text-white mt-4">HOMEPOD</p>
+                </div>
+                
+               
+            </div>
+        </div>
+</div>
--- src/Components/Footer.svelte
+++ src/Components/Footer.svelte
@@ -3,9 +3,8 @@
 </script>
 
 
-<div class="pt-72 w-full">
-
-    <footer class="bg-gray-800 pt-10 sm:mt-10 ">
+<div class="pt-72 w-full ">
+    <footer class="bg-gray-800 pt-10 sm:mt-10 fixed bottom-0 right-0 left-0">
         <div class="pt-2">
           <div class="flex pb-5 px-3 m-auto pt-5 border-t border-gray-500 text-gray-400 flex-col md:flex-row max-w-screen-lg justify-between">
              <div class="mt-2">
