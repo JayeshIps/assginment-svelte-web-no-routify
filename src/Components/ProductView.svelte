7c11844b524122613e7f645f38646dddb09128f2
Jayesh <jayeshp.itpath@gmail.com>
2023-04-10 18:37:50 +0530

    #Completed Validation changes & Worked on UI

--- src/Components/ProductView.svelte
+++ src/Components/ProductView.svelte
@@ -2,65 +2,135 @@
   import { v4 as uuidv4 } from 'uuid';
   import * as _ from 'lodash';
   import {productStore } from '../store/storeData'
-
-
   
-   // start validation form
- const formValidation = (): boolean => {
+  // start Add Product validation form
+const formValidation = (): boolean => {
+    let status:boolean=true;
     const ProductName: string = (document.getElementById("name") as HTMLInputElement).value;
     const Description: string = (document.getElementById("desc") as HTMLInputElement).value;
     const ProductPrice: string = (document.getElementById("price") as HTMLInputElement).value;
      
   
     if (ProductName === "") {
-      (document.getElementById("pro_name") as HTMLElement).innerHTML = "Is Required";
-      return false;
+      (document.getElementById("pro_name") as HTMLElement).innerHTML = "This Field Is Required";
+      status=false;
     }
-  
-    if (ProductName.length <= 3 || ProductName.length > 100) {
+    else if (ProductName.length <= 3 || ProductName.length > 100) {
       (document.getElementById("pro_name") as HTMLElement).innerHTML = "Product Name Lenght must be between 3 to 100";
-      return false;
+      status=false;
     }
-  
-    if (!isNaN(Number(ProductName))) {
+    else if (!isNaN(Number(ProductName))) {
       (document.getElementById("pro_name") as HTMLElement).innerHTML = "Product Name Allowed Only Text Character";
-      return false;
+      status=false;
     }
 
     
     if (Description === "") {
-        (document.getElementById("pro_desc") as HTMLElement).innerHTML = "Is Required";
-        return false;
+        (document.getElementById("pro_desc") as HTMLElement).innerHTML = "This Field Is Required";
+        status=false;
       }
     
-      if (Description.length <= 3 || Description.length > 150) {
+    else if (Description.length <= 3 || Description.length > 150) {
         (document.getElementById("pro_desc") as HTMLElement).innerHTML = "Description Lenght must be between 3 to 150";
-        return false;
+        status=false;
       }
     
-      if (!isNaN(Number(Description))) {
+    else if (!isNaN(Number(Description))) {
         (document.getElementById("pro_desc") as HTMLElement).innerHTML = "Description Allowed Only Text Character";
-        return false;
+        status=false;
       }
   
     if (ProductPrice === "") {
-      (document.getElementById("pro_price") as HTMLElement).innerHTML = "Is Required";
-      return false;
+      (document.getElementById("pro_price") as HTMLElement).innerHTML = "This Field Is Required";
+      status=false;
     }
-  
-    if (isNaN(Number(ProductPrice))) {
+    else if (isNaN(Number(ProductPrice))) {
       (document.getElementById("pro_price") as HTMLElement).innerHTML = "Please Enter Only Number";
-      return false;
+      status=false;
     }
-  
-    if (ProductPrice.length<= 3 || ProductPrice.length == 10) {
+    else if (ProductPrice.length<= 3 || ProductPrice.length == 10) {
       (document.getElementById("pro_price") as HTMLElement).innerHTML = "Enter Price Only 3Digit to between 10Digit Number";
-      return false;
+      status=false;
     }
-   return true;
+   
+   return status;
   };
 
-// .........End validation form..........
+ const clearformvalidation=()=>{
+  (document.getElementById("pro_name") as HTMLElement).innerHTML = "";
+  (document.getElementById("pro_desc") as HTMLElement).innerHTML = "";
+  (document.getElementById("pro_price") as HTMLElement).innerHTML = "";
+}
+
+const emptyProductAddForm=()=>{
+  (document.getElementById("name") as HTMLInputElement).value = '';
+  (document.getElementById("desc") as HTMLInputElement).value = '';
+  (document.getElementById("price") as HTMLInputElement).value = '';
+}
+// .........End Add Product validation form..........
+
+// .........Start Edit validation form.......
+function editFormvalidation():boolean{
+  let status:boolean=true;
+    const eProductName: string = (document.getElementById("ename") as HTMLInputElement).value;
+    const eDescription: string = (document.getElementById("edesc") as HTMLInputElement).value;
+    const eProductPrice: string = (document.getElementById("eprice") as HTMLInputElement).value;
+
+    if (eProductName === "") {
+      (document.getElementById("epro_name") as HTMLElement).innerHTML = "This Field Is Required";
+      status=false;
+    }
+    else if (eProductName.length <= 3 || eProductName.length > 100) {
+      (document.getElementById("epro_name") as HTMLElement).innerHTML = "Product Name Lenght must be between 3 to 100";
+      status=false;
+    }
+    else if (!isNaN(Number(eProductName))) {
+      (document.getElementById("epro_name") as HTMLElement).innerHTML = "Product Name Allowed Only Text Character";
+      status=false;
+    }
+    if (eDescription === "") {
+        (document.getElementById("epro_desc") as HTMLElement).innerHTML = "This Field Is Required";
+        status=false;
+      }
+    
+    else if (eDescription.length <= 3 || eDescription.length > 150) {
+        (document.getElementById("epro_desc") as HTMLElement).innerHTML = "Description Lenght must be between 3 to 150";
+        status=false;
+      }
+    
+    else if (!isNaN(Number(eDescription))) {
+        (document.getElementById("epro_desc") as HTMLElement).innerHTML = "Description Allowed Only Text Character";
+        status=false;
+      }
+      if (eProductPrice === "") {
+      (document.getElementById("epro_price") as HTMLElement).innerHTML = "This Field Is Required";
+      status=false;
+    }
+  
+    else if (isNaN(Number(eProductPrice))) {
+      (document.getElementById("epro_price") as HTMLElement).innerHTML = "Please Enter Only Number";
+      status=false;
+    }
+  
+    else if (eProductPrice.length<= 3 || eProductPrice.length == 10) {
+      (document.getElementById("epro_price") as HTMLElement).innerHTML = "Enter Price Only 3Digit to between 10Digit Number";
+      status=false;
+    }
+    return status;
+}
+
+const clearEditFormvalidation=()=>{
+  (document.getElementById("epro_name") as HTMLElement).innerHTML = "";
+  (document.getElementById("epro_desc") as HTMLElement).innerHTML = "";
+  (document.getElementById("epro_price") as HTMLElement).innerHTML = "";
+}
+
+const emptyEditProductForm=()=>{
+  (document.getElementById("ename") as HTMLInputElement).value = '';
+  (document.getElementById("edesc") as HTMLInputElement).value = '';
+  (document.getElementById("eprice") as HTMLInputElement).value = '';
+}
+// .........End Edit validation form.......
   
 let addproduct:boolean=false;
 function toggleAddProduct(){
@@ -87,6 +157,7 @@ type Product={
 
 const addProduct = () => 
 {
+
   if (formValidation()) {
     const id: string = uuidv4();
     const name: string = (document.getElementById("name") as HTMLInputElement).value;
@@ -95,13 +166,13 @@ const addProduct = () =>
     const product = {id: uuidv4(),name, desc, price };
     productStore.update(products => [...products, product]);
     toggleAddProduct();
+    clearformvalidation();
+    emptyProductAddForm();
   }
-  (document.getElementById("name") as HTMLInputElement).value = '';
-  (document.getElementById("desc") as HTMLInputElement).value = '';
-  (document.getElementById("price") as HTMLInputElement).value = '';
+ 
 };
-
 //.........End Add data in table...........
+
 
 
 //.........Start Edit data in table...........
@@ -131,14 +202,18 @@ function updateProduct(id:string,name:string){
 }
 
 const saveProduct = () => {
-  
+  if (editFormvalidation()) {
   productStore.update(products=>{
     let updatedProduct = _.find(products, { id:productid.value }) as Product;
     updatedProduct.name=editname.value
     updatedProduct.desc=editdesc.value
     updatedProduct.price=editprice.value
+    clearEditFormvalidation();
+    emptyEditProductForm();
     return products;
   })
+  toggleEditProduct();
+}
 }
 //.........End Edit data in table...........
 
@@ -161,7 +236,7 @@ const deleteProduct = (id: string,name:string):void => {
 
 <div class="w-9/12">
   <div class="flex justify-end ml-44 w-full pt-44">
-    <button class="bg-blue-500  hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" on:click={toggleAddProduct}>
+    <button class="bg-blue-500  hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"on:click|preventDefault={()=>{toggleAddProduct();}}>
       Add Products
     </button>
   </div>
@@ -195,7 +270,7 @@ const deleteProduct = (id: string,name:string):void => {
                   <p class="text-gray-500 text-sm font-semibold tracking-wide">{item.price}</p>
                 </td>
                 <td class="px-6 py-4">
-                  <button class="btn bg-yellow-400 text-black font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" on:click={() => {toggleEditProduct(); updateProduct(item.id,item.name);}} type="button">
+                  <button type="button" class="btn bg-yellow-400 text-black font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" on:click={() => {toggleEditProduct(); updateProduct(item.id,item.name);}}>
                     Edit
                   </button>
                   <button class="btn bg-red-600 text-black font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" on:click={() => {toggleDeleteProduct(); deleteProduct(item.id,item.name) }} type="button">
@@ -214,7 +289,7 @@ const deleteProduct = (id: string,name:string):void => {
 <div class="{addproduct ? ' ':'hidden'}  h-screen top-0 left-0 bottom-0 right-0  fixed  backdrop-blur-md">
 <div class="pt-10  ml-auto mr-auto w-full h-full max-w-md md:h-auto  ">
   
-  <form class="bg-white shadow-md rounded border-8 border-double pt-10  w-full" on:submit|preventDefault={formValidation} on:submit|preventDefault={addProduct}>
+  <form class="bg-white shadow-md rounded border-8 border-double pt-10  w-full" action="#" >
     <p class="text-black font-bold text-2xl pb-5 ml-5">Add New Product</p>
     <div class="mb-4">
       <label class="block text-gray-700 text-sm font-bold mb-2 ml-5" for="">
@@ -241,10 +316,10 @@ const deleteProduct = (id: string,name:string):void => {
     </div>
     
     <div class="flex justify-end gap-2 pb-5 ">
-      <button class="btn bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit"  on:click={formValidation} on:click|preventDefault={addProduct}>
+      <button class="btn bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit"   on:click|preventDefault={()=>{formValidation();addProduct();}}>
         Add
       </button>
-      <button on:click={toggleAddProduct} class="btn bg-white text-black font-italic py-2 px-4 mr-4 rounded focus:outline-none focus:shadow-outline" type="button">
+      <button  on:click|preventDefault={()=>{toggleAddProduct();clearformvalidation();emptyProductAddForm();}} class="btn bg-white text-black font-italic py-2 px-4 mr-4 rounded focus:outline-none focus:shadow-outline" type="button">
         Cancel    
       </button>
     </div>
@@ -258,7 +333,7 @@ const deleteProduct = (id: string,name:string):void => {
 <div class="{deleteproduct ? ' ':'hidden'} w-full h-screen top-0 left-0 bottom-0 right-0 fixed  backdrop-blur-md">
   <div class="pt-10   h-full ml-auto mr-auto w-full max-w-md md:h-auto  ">
     
-    <form class="bg-white shadow-md rounded border-8 border-double ">
+    <form class="bg-white shadow-md rounded border-8 border-double " action="#">
       <p class="text-black font-bold text-2xl py-5 ml-5">Delete -{delProductName} </p>
       <div class="mb-4">
         <label class="block text-gray-700 text-sm font-bold mb-2 ml-5" for="">
@@ -285,39 +360,39 @@ const deleteProduct = (id: string,name:string):void => {
   <div class="{editproduct ? ' ':'hidden'}  h-screen top-0 left-0 bottom-0 right-0  fixed  backdrop-blur-md">
     <div class="pt-10  ml-auto mr-auto w-full h-full max-w-md md:h-auto  ">
       
-      <form class="bg-white shadow-md rounded border-8 border-double pt-10  w-full" on:submit|preventDefault={formValidation}>
+      <form class="bg-white shadow-md rounded border-8 border-double pt-10  w-full" action="#">
         <p class="text-black font-bold text-2xl pb-5 ml-5">Edit Product-{editProductName}</p>
         <div class="mb-4">
           <label class="block text-gray-700 text-sm font-bold mb-2 ml-5" for="">
             Name
           </label>
-          <input class="shadow basepearance-none border rounded  ml-5 py-2 w-11/12 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="name" bind:value={editname.value} type="text" placeholder="EnterProduct">
+          <input class="shadow basepearance-none border rounded  ml-5 py-2 w-11/12 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="ename" bind:value={editname.value} type="text" placeholder="EnterProduct">
           <input class="shadow basepearance-none border rounded  ml-5 py-2 w-11/12 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="productid" bind:value={productid.value} type="text" placeholder="EnterProduct" hidden>
-          <span id="pro_name" class="text-red-600 text-base ml-5"></span>
+          <span id="epro_name" class="text-red-600 text-base ml-5"></span>
         </div>
         
         <div class="mb-6">
           <label class="block text-gray-700 text-sm font-bold mb-2 ml-5" for="">
             Description
           </label>
-          <textarea class="shadow appearance-none border rounded  ml-5 py-2 w-11/12 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="desc" bind:value={editdesc.value} placeholder="EnterDescription" rows="4"></textarea>
-          <span id="pro_desc" class="text-red-600 text-base ml-5"></span>
+          <textarea class="shadow appearance-none border rounded  ml-5 py-2 w-11/12 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="edesc" bind:value={editdesc.value} placeholder="EnterDescription" rows="4"></textarea>
+          <span id="epro_desc" class="text-red-600 text-base ml-5"></span>
         </div>
         
         <div class="mb-6">
           <label class="block text-gray-700 text-sm font-bold mb-2 ml-5" for="">
             Price
           </label>
-          <input class="shadow appearance-none border rounded  ml-5 py-2 w-11/12 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="price" type="text" bind:value={editprice.value} placeholder="EnterPrice">
-          <span id="pro_price" class="text-red-600 text-base ml-5"></span>
+          <input class="shadow appearance-none border rounded  ml-5 py-2 w-11/12 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="eprice" type="text" bind:value={editprice.value} placeholder="EnterPrice">
+          <span id="epro_price" class="text-red-600 text-base ml-5"></span>
         </div>
         
         <div class="flex justify-end gap-2 pb-5 ">
           <!-- svelte-ignore missing-declaration -->
-          <button class="btn bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit" id="editId"  on:click={formValidation} on:click|preventDefault={()=>{toggleEditProduct();saveProduct()}}>
+          <button class="btn bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit" id="editId"  on:click|preventDefault={()=>{saveProduct();formValidation();}}>
             Update
           </button>
-          <button on:click={toggleEditProduct} class="btn bg-white text-black font-italic py-2 px-4 mr-4 rounded focus:outline-none focus:shadow-outline" on:click={formValidation} type="button">
+          <button on:click={()=>{toggleEditProduct();emptyEditProductForm();clearEditFormvalidation();}} class="btn bg-white text-black font-italic py-2 px-4 mr-4 rounded focus:outline-none focus:shadow-outline" type="button">
             Cancel    
           </button>
         </div>
